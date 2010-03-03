3e97f9224f97889f4c18130a9d0a90d08577a2a5
Maurice <mauricemulder@gmail.com>
2010-03-03 09:33:27 +0100

    vet

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ int main(int argc, char ** argv){
       return 0;
     }
   }
-  VlSiftFilt * a =  vl_sift_new(10,10,5,5,0);
+  VlSiftFilt * a =  vl_sift_new(100,100,5,5,0);
+  vl_sift_pix image [100*100];
+  vl_sift_pix desc [2*100*100];
+  vl_sift_process_first_octave(a, image);
   //vl_sift_calc_raw_descriptor();
-  Parameters * p = Parameters::getInstance();
-  p->readFile((char *) "parameters.xml");
-  start(argc, argv);
-
+  //Parameters * p = Parameters::getInstance();
+  //p->readFile((char *) "parameters.xml");
+  //start(argc, argv);
+  
 }
 
 void Libraries(){
