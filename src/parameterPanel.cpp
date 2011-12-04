9136985d48ab09abed43961d3815a8cdf3ceb9cf
Jelmer <jelmer@ikhoefgeen.nl>
2011-12-04 12:45:06 +0100

    Group parameters by type

--- src/parameterPanel.cpp
+++ src/parameterPanel.cpp
@@ -1,4 +1,6 @@
 #include "parameterPanel.h"
+
+#include "parameters.h"
 #include <map>
 #include <string>
 
@@ -10,31 +12,37 @@ ParameterPanel::ParameterPanel()
   Parameters *parameters = Parameters::getInstance();
   panel = GLUI_Master.create_glui("parameters", 0, 1325, 150);
 
-  setupVariables(parameters->floatParameters, GLUI_EDITTEXT_FLOAT);
+  setupVariables("Float variables",
+    parameters->floatParameters, GLUI_EDITTEXT_FLOAT);
   
   panel->add_column(true);
   
-  setupVariables(parameters->intParameters, GLUI_EDITTEXT_INT);
+  setupVariables("Integer variables",
+    parameters->intParameters, GLUI_EDITTEXT_INT);
 }
 
 template <class T>
-void ParameterPanel::setupVariables(std::map<std::string, T> &parameters, int data_type){
+GLUI_Panel *ParameterPanel::setupVariables(std::string const &name,
+  std::map<std::string, T> &parameters, int data_type)
+{
   int total;
+  GLUI_Panel *group = panel->add_panel(name.c_str());
   
   for (typename std::map<std::string, T>::iterator it = parameters.begin();
     it != parameters.end(); ++it)
   {
-    if(total > 15)
+    if (total++ > 15)
     {
       total = 0;
-      panel->add_column(false);
+      panel->add_column_to_panel(group, false);
     }
-    else
-      total++;
     
     fields.push_back(
-      panel->add_edittext(it->first.c_str(), data_type, (void *) &it->second));
+      panel->add_edittext_to_panel(group, it->first.c_str(),
+        data_type, (void *) &it->second));
   }
+
+  return group;
 }
 
 }}
--- src/parameterPanel.h
+++ src/parameterPanel.h
@@ -1,9 +1,10 @@
 #ifndef PARAMETERPANEL_H
 #define PARAMETERPANEL_H
 
-#include "parameters.h"
 #include "../libs/glui/glui.h"
 #include <vector>
+#include <map>
+#include <string>
 
 namespace vito{
 namespace gui{
@@ -12,11 +13,12 @@ class ParameterPanel
 {
 
 private:
-  std::vector<GLUI_EditText*> fields;
   GLUI *panel;
+  std::vector<GLUI_EditText *> fields;
   
   template <class T>
-  void setupVariables(std::map<std::string, T> &parameters, int data_type);
+  GLUI_Panel *setupVariables(std::string const &name,
+    std::map<std::string, T> &parameters, int data_type);
 
 public:
   ParameterPanel();
